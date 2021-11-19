803a77d9de8e26e87038c1431a4587b2f9623e4a
Mitch Lui <mitch@mitchlui.dev>
2021-11-19 15:08:33 +0000

    naming changes

--- server/tone_analyzer.py
+++ server/tone_analyzer.py
@@ -1,13 +1,12 @@
 from ibm_watson import ToneAnalyzerV3
 from dotenv import load_dotenv
 import os
-import json
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator, authenticator
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-class ToneAnalyzerSPE:
+class IBMToneAnalyzer:
     def __init__(self) -> None:
         load_dotenv()
-        self.authenticator = IAMAuthenticator(os.environ['KEY'])
+        self.authenticator = IAMAuthenticator(os.getenv('KEY'))
         self.tone_analyzer = ToneAnalyzerV3(
             version='2017-09-21',
             authenticator=self.authenticator
@@ -15,7 +14,6 @@ class ToneAnalyzerSPE:
         self.tone_analyzer.set_service_url('https://api.eu-gb.tone-analyzer.watson.cloud.ibm.com/instances/cacd7ebe-969c-4c32-a8e3-fb03cdedb442')
 
     def get_analysis(self, text: str) -> dict:
-
         return self.tone_analyzer.tone(
             {'text':text},
             content_type = 'application/json',
