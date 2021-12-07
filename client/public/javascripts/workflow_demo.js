9acae206a2202ae7a9d0f660a312655427868f83
Mitch Lui <mitch@mitchlui.dev>
2021-12-07 12:38:18 +0000

    removed $

--- client/public/javascripts/workflow_demo.js
+++ client/public/javascripts/workflow_demo.js
@@ -1,10 +1,10 @@
 function runWorkflowDemo() {
   console.log('Running workflow demo...');
-  var $formData = $('form').serializeArray();
+  var formData = $('form').serializeArray();
   var formResult;
   fetch('http://localhost:5001/twitterapi/tweets', {
   method: 'POST',
-  body: $formData,
+  body: formData,
   headers: {'Content-type': 'application/json;charset=UTF-8'}
   })
     .then(response => response.json()) 
