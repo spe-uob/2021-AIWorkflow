138bcfce36266140dad32583d98449a81b48005a
michael_poluektov <michael.poluektov@gmail.com>
2022-02-04 16:05:52 +0100

    Added login component

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {GoogleLogin} from 'react-google-login'
+
+const clientId = 'YOUR_CLIENT_ID.apps.googleusercontent.com';
+
+function Login() {
+    const onSuccess = (res) => {
+        console.log('[Login Success] currentUser:', res.profileObj);
+    };
+
+    const onFailure = (res) => {
+        console.log('[Login failed] res:', res);
+    };
+
+    return (
+        <div>
+            <GoogleLogin
+                clientId={clientId}
+                buttonText="Login"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                cookiePolicy={'single_host_origin'}
+                style={{marginTop: '100px'}}
+                isSignedIn={true}
+            />
+        </div>
+    );
+}
+
+export default Login;
