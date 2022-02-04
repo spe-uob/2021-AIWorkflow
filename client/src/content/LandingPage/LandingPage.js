2d629dd50fee109fda07fed445726dac3725cf09
JeffreyLu1972543 <tr19097@bristol.ac.uk>
2022-02-04 18:33:41 +0000

    2.4 version

--- client/src/content/LandingPage/LandingPage.js
+++ client/src/content/LandingPage/LandingPage.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import {Button} from 'carbon-components-react';
+import bgvideo from './bg-video.mp4'
+import './LandingPage.css';
+
 
 const LandingPage = () => {
-  return <div>LANDING PAGE</div>;
+  return (
+    <div className="video-container">
+     <video autoPlay loop muted>
+      <source src={bgvideo} type="video/mp4"/>
+     </video>
+     <div className="caption">
+        <h1>AI WORKFLOW INTEGRATION</h1>
+        <Button>
+          GET STARTED
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
