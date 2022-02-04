20c27e2bd4ff81fd5a778dbebd9e7c557909d4f9
michael_poluektov <michael.poluektov@gmail.com>
2022-02-04 16:20:33 +0100

    Added login page to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,15 +9,16 @@ import WorkflowDemoPage from './content/WorkflowDemoPage';
 class App extends Component {
   render() {
     return (
-      <>
+      <div>
         <AIWorkflowHeader />
         <Content>
           <Routes>
             <Route exact path="/" element={<LandingPage/ >} />
             <Route path="/workflow_demo" element={<WorkflowDemoPage/>} />
+            <Route path="/login" element={<LoginPage/>}/>
           </Routes>
         </Content>
-      </>
+      </div>
     );
   }
 }
---
+++ client/src/content/LoginPage/LoginPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return <div>LOGIN PAGE</div>;
+};
+
+export default LoginPage;
