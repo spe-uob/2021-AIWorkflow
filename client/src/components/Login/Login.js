da57ed4045b7bb24e57b04e11a07f1f8b66c414c
michael_poluektov <michael.poluektov@gmail.com>
2022-02-05 14:46:09 +0100

    Styling works now, changed login to hooks, added icon

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,30 +1,39 @@
 import React from 'react';
-import {GoogleLogin} from 'react-google-login'
+import { useGoogleLogin } from 'react-google-login';
 
 const clientId = '516108771432-8r055agb6k336emqdqh242s4c73lduf7.apps.googleusercontent.com';
 
+
 function Login() {
-    const onSuccess = (res) => {
-        console.log('[Login Success] currentUser:', res.profileObj);
-    };
-
-    const onFailure = (res) => {
-        console.log('[Login failed] res:', res);
-    };
-
-    return (
-        <div>
-            <GoogleLogin
-                clientId={clientId}
-                buttonText="Login"
-                onSuccess={onSuccess}
-                onFailure={onFailure}
-                cookiePolicy={'single_host_origin'}
-                style={{marginTop: '100px'}}
-                isSignedIn={true}
-            />
-        </div>
+  const onSuccess = (res) => {
+    console.log('Login Success: currentUser:', res.profileObj);
+    alert(
+      `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
     );
+  };
+
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+    alert(
+      `Failed to login.`
+    );
+  };
+
+  const { signIn } = useGoogleLogin({
+    onSuccess,
+    onFailure,
+    clientId,
+    isSignedIn: true,
+    accessType: 'offline',
+  });
+
+  return (
+    <button onClick={signIn} className="button">
+      <img src="icons/google.svg" alt="google login" className="icon"></img>
+
+      <span className="buttonText">Sign in with Google</span>
+    </button>
+  );
 }
 
 export default Login;
--- client/src/content/LoginPage/LoginPage.js
+++ client/src/content/LoginPage/LoginPage.js
@@ -3,13 +3,15 @@ import Login from '../../components/Login/Login';
 import Logout from '../../components/Logout/Logout';
 
 const LoginPage = () => {
-  return <div>
+  return <div className="App">
     <div>    
       <h3>You are logged out.</h3>
     </div>
+    <br/>
     <div>
       <Login/>
     </div>
+    <br/>
     <div >
       <Logout/>
     </div>
