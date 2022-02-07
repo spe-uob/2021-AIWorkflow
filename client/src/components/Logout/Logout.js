88c4e545a962997cf5a38cc13d847e231e86a9f8
michael_poluektov <michael.poluektov@gmail.com>
2022-02-07 18:16:44 +0100

    Made logout actually log the user out

--- client/src/components/Logout/Logout.js
+++ client/src/components/Logout/Logout.js
@@ -6,6 +6,7 @@ const clientId = '516108771432-8r055agb6k336emqdqh242s4c73lduf7.apps.googleuserc
 function LogoutHooks() {
   const onLogoutSuccess = (res) => {
     console.log('Logged out Success');
+    sessionStorage.removeItem('sessionObj');
     alert('Logged out Successfully.');
   };
 
