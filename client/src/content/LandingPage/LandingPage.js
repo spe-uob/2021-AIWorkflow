ef18e7004bd19313a13d436d6b5621d3d93d4006
JeffreyLu1972543 <tr19097@bristol.ac.uk>
2022-02-11 14:12:02 +0000

    commented video css

--- client/src/content/LandingPage/LandingPage.js
+++ client/src/content/LandingPage/LandingPage.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {Button} from 'carbon-components-react';
-// import bgvideo from './bg-video1.mp4'
+ import bgvideo from './bg-video1.mp4'
 import './LandingPage.css';
-import bgpicture from "./background.jpeg"
+// import bgpicture from "./background.jpeg"
 
 
 const LandingPage = () => {
   return (
     <div className="background-container">
-     {/* <video autoPlay loop muted>
+     <video autoPlay loop muted>
       <source src={bgvideo} type="video/mp4"/>
-     </video> */}
-     <img src={bgpicture} alt="logo" />
+     </video>
+     {/* <img src={bgpicture} alt="logo" /> */}
      <div className="caption">
         <h1 style={{fontWeight: "bold",fontSize:"50px"}}>
           AI WORKFLOW INTEGRATION
