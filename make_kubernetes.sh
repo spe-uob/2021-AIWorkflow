be3e7133305b4acddfc80482d7f2cd959bb5f963
Mitch Lui <mitch@mitchlui.dev>
2022-02-15 20:38:04 +0000

    typo

--- make_kubernetes.sh
+++ make_kubernetes.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ]
     echo "No arguments supplied -- check what kubectl contexts you have (kubectl config get-contexts) and use that (kubectl config set-context <context>)"
     exit 1
 fi
-kubectl context use-context $1
+kubectl config use-context $1
 kubectl apply -f backend-service.yaml,backend-deployment.yaml,frontend-tcp-service.yaml,frontend-deployment.yaml,database-claim0-persistentvolumeclaim.yaml,database-deployment.yaml,database-service.yaml
 kubectl get pods
 exit 0
