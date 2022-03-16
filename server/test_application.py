daf4164be40d2fbe6bf47a843f84c4815dc13fab
SLZ0106 <gp19665@bristol.ac.uk>
2022-03-16 18:52:02 +0000

    add a simple test in test_application.py linking to application.py

--- server/test_application.py
+++ server/test_application.py
@@ -1,2 +1,8 @@
-def test_hello_world():
-    return 2 + 2 == 4
+from itsdangerous import json
+import pytest
+from server import application
+
+json = application.JSONResponse
+
+def test_api():
+    assert json == {"message": "app is running", "success": True}
