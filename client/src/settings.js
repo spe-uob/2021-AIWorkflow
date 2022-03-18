61db54f808d33e50ea9dc4c1873414fbed5cd649
Mitch Lui <mitch@mitchlui.dev>
2022-03-18 13:33:02 +0000

    Added API_DOMAIN to constants

--- client/src/settings.js
+++ client/src/settings.js
@@ -1,4 +1,5 @@
 const Constants = {
-    CLIENT_ID: '516108771432-k0ifm1hkdanslpbd44tojjqehni63bj5.apps.googleusercontent.com'
+    CLIENT_ID: '516108771432-k0ifm1hkdanslpbd44tojjqehni63bj5.apps.googleusercontent.com',
+    API_DOMAIN: 'http://localhost:5001',
   }
   export default Constants
