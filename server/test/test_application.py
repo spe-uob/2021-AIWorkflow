c0208ee7499b0b0172c00f4ebb96ad35a506a59b
SLZ0106 <gp19665@bristol.ac.uk>
2022-03-18 14:59:56 +0000

    change the path of test

--- server/test/test_application.py
+++ server/test/test_application.py
@@ -1,7 +1,8 @@
-from itsdangerous import json
+from email.mime import application
 import pytest
-from .application import app
+from ..application import app
 from fastapi.testclient import TestClient
+
 
 client = TestClient(app)
 def test_root():
