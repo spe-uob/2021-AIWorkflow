14fc0d10df427a8a8a8630f473a15d84f625eae2
JeffreyLu1972543 <tr19097@bristol.ac.uk>
2022-03-28 19:06:49 +0100

    last-modify

--- client/src/components/Header/AIWorkflowHeader.js
+++ client/src/components/Header/AIWorkflowHeader.js
@@ -58,7 +58,7 @@ const AIWorkflowHeader = () => (
               </HeaderMenuItem>
             </HeaderSideNavItems>
           </SideNavItems>
-          <SideNavItems>
+          < SideNavItems>
             <HeaderSideNavItems>
               <HeaderMenuItem element={Link} to="/Workflow">
                 Workflow
