bad7116f56c7794c7f9f647f2f5e7e6d5fa0e142
SLZ0106 <gp19665@bristol.ac.uk>
2022-03-28 19:56:58 +0100

    Add more schemas

--- server/routers/workflow_schemas.py
+++ server/routers/workflow_schemas.py
@@ -4,9 +4,9 @@ from pyparsing import Dict
 from pydantic import BaseModel
 
 class WorkflowModel(BaseModel):
-    searchtwitter : Dict["text": str, "startime": str, "endtime": str]
+    searchtwitter : Dict["text": str, "keywords": List[str], "tones": List[str], "startime": str, "endtime": str]
     
 
 class Workflow(BaseModel):
     user_id: str
-    workflow: list[WorkflowModel]
+    workflow: List[WorkflowModel]
