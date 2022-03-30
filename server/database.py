2f62969e69fd1dd9574c39bdc14255ca2eca170c
michael_poluektov <michael.poluektov@gmail.com>
2022-03-30 16:08:54 +0200

    DRY

--- server/database.py
+++ server/database.py
@@ -5,6 +5,7 @@ MONGO_DETAILS = "mongodb://localhost:27017"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 database = client.ibm
 tweet_collection = database.get_collection("tweets")
+print(type(tweet_collection))
 workflow_collection = database.get_collection("workflows")
 
 def tweet_helper(tweet) -> dict:
@@ -24,12 +25,7 @@ async def retrieve_all_tweets() -> dict:
     return tweets
 
 # Use json.loads() to convert json string to dict
-async def add_tweet(tweet_data: dict) -> dict:
-    tweet = await tweet_collection.insert_one(tweet_data)
-    new_tweet = await tweet_collection.find_one({"_id": tweet.inserted_id})
-    return tweet_helper(new_tweet)
-
-async def add_workflow(workflow_data: dict) -> dict:
-    workflow = await workflow_collection.insert_one(workflow_data)
-    new_workflow = await workflow_collection.find_one({"_id": workflow.inserted_id})
-    return new_workflow
+async def add_to_collection(data: dict, collection: motor.motor_asyncio.AsyncIOMotorCollection) -> dict:
+    item = await collection.insert_one(data)
+    new_data = await collection.find_one({"_id": item.inserted_id})
+    return new_data
