688e8d53dc9153fcc4d42e6ffd23309db540ae75
JeffreyLu1972543 <tr19097@bristol.ac.uk>
2022-03-31 20:41:27 +0100

    added-button-use-carbonDesign

--- client/src/content/WorkflowPage/WorkflowPage.js
+++ client/src/content/WorkflowPage/WorkflowPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useRete } from "./rete";
-
+import {Button}  from 'carbon-components-react'
 
 function Editor() {
   const [setContainer] = useRete();
@@ -18,11 +18,17 @@ function Editor() {
 
 function WorkflowPage() {
   const [visible, setVisible] = useState(true);
-
+  
   return (
-    <div className="Workflow-Page">
+    
+    <div className="Workflow-Page" >
       {/* <button onClick={() => setVisible(false)}>Destroy</button>
       <button onClick={() => setVisible(true)}>Restore</button> */}
+      <Button style={{position:"fixed",bottom:"35px",right:"35px",width:"150px",height:"50px",display:"block",
+whiteSpace:"nowrap",overflow:"hidden"}}>Run workflow</Button>
+    
+
+
       {visible && <Editor />}
     </div>
   );
