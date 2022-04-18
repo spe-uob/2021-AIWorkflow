ac211718e2d1e5f92bf448ebfb12b7560e8d5dcf
SLZ0106 <gp19665@bristol.ac.uk>
2022-04-18 11:43:26 +0100

    add auth key to parse_workflow

--- server/routers/internal/workflow.py
+++ server/routers/internal/workflow.py
@@ -36,8 +36,10 @@ class WorkflowNew:
         else:
             self.parse_workflow(workflow_request)
   
-    def parse_workflow(self, workflow_request: Dict[str, str]) -> None:
+    def parse_workflow(self, backend_auth_code: str, workflow_request: Dict[str, str]) -> None:
         workflow_request = workflow_request["nodes"]
+        if backend_auth_code != self.users.get_users["code"]:
+            print("Auth_key incorrect!")
         for id, node in workflow_request.items():
             #print(id, node)
             if node["name"] == "Search Twitter":
