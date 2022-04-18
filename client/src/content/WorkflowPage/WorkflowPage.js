d99de58ddfc3b65c3b87d47e8e430c037402ef84
JeffreyLu1972543 <tr19097@bristol.ac.uk>
2022-04-18 13:53:47 +0100

    use-sessionStorage-to-pass-workflow

--- client/src/content/WorkflowPage/WorkflowPage.js
+++ client/src/content/WorkflowPage/WorkflowPage.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useRete } from "./rete";
 import {Button}  from 'carbon-components-react';
 import "./_workflow-page.scss";
+//import { MyNode } from "./MyNode";
+
 
 function Editor() {
   const [setContainer] = useRete();
@@ -13,13 +15,15 @@ function Editor() {
     />
   );
 }
-
+function handleClick(){
+    console.log(JSON.parse(sessionStorage.getItem("workflowObj")));
+}
 function WorkflowPage() {
   const [visible,] = useState(true);
   
   return (
     <div className="workflow-page" >
-      <Button className="run-workflow-button">Run workflow</Button>
+      <Button onClick={handleClick} className="run-workflow-button">Run workflow</Button>
       {visible && <Editor />}
     </div>
   );
