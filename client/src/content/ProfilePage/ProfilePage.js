473dafc2724e6da888466cc0707c35e887f4d1f9
JeffreyLu1972543 <tr19097@bristol.ac.uk>
2022-05-03 22:03:35 +0100

    add-content-class-in-ProfilePage

--- client/src/content/ProfilePage/ProfilePage.js
+++ client/src/content/ProfilePage/ProfilePage.js
@@ -3,6 +3,8 @@ import Login from '../../components/Login/Login';
 import Logout from '../../components/Logout/Logout';
 import cookie from "json-cookie";
 
+
+
 const ProfilePage = () => {
   console.log(cookie.get("googleObj"));
   if (cookie.get("googleObj") === "") {
@@ -12,33 +14,35 @@ const ProfilePage = () => {
         <div className={"background"}>
           <img src={"./login-background.png"} alt={"lol"}/>
         </div>
-        <div className="login-text">
-          <h1>Log in to IBM <span className={"bold"}>AIWorkflow</span></h1>
-        </div>
-        <div className={"login-with-google"}>
-          <Login/>
+        <div className={"content"}>
+          <div className="login-text">
+            <span>Log in to IBM <span className={"bold"}>AIWorkflow</span></span>
+          </div>
+          <div className={"login-with-google"}>
+            <Login/>
+          </div>
         </div>
       </div>
       </>
-    )}
-    else {
-      var picture = cookie.get("googleObj").picture;
-      var email = cookie.get("googleObj").email;
-      var name = cookie.get("googleObj").name;
-      return (
-        <div className="profile-container">
-          <div className={"background"}>
-            <img src={"./login-background.png"} alt={"lol"}/>
-          </div>
-          <div>
-            <img className={"profile-pic"} src={picture} alt="logo" />
-            <p><span className={"bold"}>Name: </span>{name}</p>
-            <p><span className={"bold"}>Email: </span> {email}</p>
-            <Logout/>
-          </div>
+    )
+  } else {
+    var picture = cookie.get("googleObj").picture;
+    var email = cookie.get("googleObj").email;
+    var name = cookie.get("googleObj").name;
+    return (
+      <div className="profile-container">
+        <div className={"background"}>
+          <img src={"./login-background.png"} alt={"lol"}/>
         </div>
-      )
-    }
+        <div className={"content"}>
+          <img className={"profile-pic"} src={picture} alt="logo" />
+          <p><span className={"bold"}>Name: </span>{name}</p>
+          <p><span className={"bold"}>Email: </span> {email}</p>
+          <Logout/>
+        </div>
+      </div>
+    )
+  }
 };
 
 export default ProfilePage;
