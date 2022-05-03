85aa32adb619640945cd8b58af6b76acc154b3bd
JeffreyLu1972543 <tr19097@bristol.ac.uk>
2022-05-03 17:24:55 +0100

    profilepage-login

--- client/src/content/ProfilePage/ProfilePage.js
+++ client/src/content/ProfilePage/ProfilePage.js
@@ -4,7 +4,23 @@ import Logout from '../../components/Logout/Logout';
 import cookie from "json-cookie";
 
 const ProfilePage = () => {
-  
+  console.log(cookie.get("googleObj"));
+  if (cookie.get("googleObj") === "") {
+    return (
+      <>
+      <div className={"login-container"}>
+        <div className={"background"}>
+          <img src={"./login-background.png"} alt={"lol"}/>
+        </div>
+        <div className="login-text">
+          <h1>Log in to IBM <span className={"bold"}>AIWorkflow</span></h1>
+        </div>
+        <div className={"login-with-google"}>
+          <Login/>
+        </div>
+      </div>
+      </>
+    )}
 };
 
 export default ProfilePage;
