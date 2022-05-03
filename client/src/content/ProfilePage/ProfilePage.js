efcfc62aace107adfaf08cf14cd776e1161c1c2a
JeffreyLu1972543 <tr19097@bristol.ac.uk>
2022-05-03 16:52:10 +0100

    Add-ProfilePage.js

---
+++ client/src/content/ProfilePage/ProfilePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Login from '../../components/Login/Login';
+import Logout from '../../components/Logout/Logout';
+import cookie from "json-cookie";
+
+const ProfilePage = () => {
+  
+};
+
+export default ProfilePage;
